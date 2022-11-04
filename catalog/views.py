c2157f02fc9c9472023ae0ab5fd8d36cbbf726bd
Badrudeen Abdul-hameed <adex.badr18@gmail.com>
2022-11-04 10:45:27 +0100

    Count number of visits to index page with sessions

--- catalog/views.py
+++ catalog/views.py
@@ -27,6 +27,10 @@ def index(request):
     book_filter = Book.objects.filter(
         title__contains='man').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -34,6 +38,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'book_filter': book_filter,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
