fd039025941af511154d56ffa90dfc43d92c8844
Badrudeen Abdul-hameed <adex.badr18@gmail.com>
2022-11-15 04:21:15 +0100

    Modify DEBUG & SECRET_KEY settings for production

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3+n)mi$=t$rypumfc1aii&*)x%u97co1zu9z*!8kyw#^6*3giu'
+# SECRET_KEY = 'django-insecure-3+n)mi$=t$rypumfc1aii&*)x%u97co1zu9z*!8kyw#^6*3giu'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-3+n)mi$=t$rypumfc1aii&*)x%u97co1zu9z*!8kyw#^6*3giu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
